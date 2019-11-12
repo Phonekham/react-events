2f7b793c87394a151d6855af3f306a62b8e14ba7
Phonekham <phone.madridista@gmail.com>
2019-11-12 13:45:37 +0700

    setup

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Button, Icon } from "semantic-ui-react";
+
+import EventDashboard from "../../features/event/EventDashboard/EventDashboard";
+
+function App() {
+  return (
+    <div>
+      <EventDashboard></EventDashboard>
+    </div>
+  );
+}
+
+export default App;
