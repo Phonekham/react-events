1c3e5af76ef1fcbcd9ef8adfd6ee8540365a163e
Phonekham <phone.madridista@gmail.com>
2019-11-17 14:15:29 +0700

    Internal navigation for application

--- src/features/user/Settings/SettingsDashboard.jsx
+++ src/features/user/Settings/SettingsDashboard.jsx
@@ -1,10 +1,29 @@
 import React from "react";
+import { Grid } from "semantic-ui-react";
+import { Route, Redirect, Switch } from "react-router";
+
+import SettingsNav from "./SettingsNav";
+import BasicPage from "./BasicPage";
+import AboutPage from "./AboutPage";
+import PhotosPage from "./PhotosPage";
+import AccountPage from "./AccountPage";
 
 const SettingsDashboard = () => {
   return (
-    <div>
-      <h1>settings dashboard</h1>
-    </div>
+    <Grid>
+      <Grid.Column width={12}>
+        <Switch>
+          <Redirect exact from="/settings" to="settings/basic"></Redirect>
+          <Route path="/settings/basic" component={BasicPage}></Route>
+          <Route path="/settings/about" component={AboutPage}></Route>
+          <Route path="/settings/photos" component={PhotosPage}></Route>
+          <Route path="/settings/account" component={AccountPage}></Route>
+        </Switch>
+      </Grid.Column>
+      <Grid.Column width={4}>
+        <SettingsNav></SettingsNav>
+      </Grid.Column>
+    </Grid>
   );
 };
 
