cf7c14fa2816d798d9b32bea38daff6923b294fb
Phonekham <phone.madridista@gmail.com>
2019-11-18 16:06:11 +0700

    Adding static data for the event detailed page

--- src/features/event/EventDetailed/EventDetailedPage.jsx
+++ src/features/event/EventDetailed/EventDetailedPage.jsx
@@ -6,16 +6,43 @@ import EventDetailInfo from "./EventDetailInfo";
 import EventDetailChat from "./EventDetailChat";
 import EventDetailedSidebar from "./EventDetailedSidebar";
 
+const event = {
+  id: "1",
+  title: "Trip to Tower of London",
+  date: "2018-03-27",
+  category: "culture",
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.",
+  city: "London, UK",
+  venue: "Tower of London, St Katharine's & Wapping, London",
+  hostedBy: "Bob",
+  hostPhotoURL: "https://randomuser.me/api/portraits/men/20.jpg",
+  attendees: [
+    {
+      id: "a",
+      name: "Bob",
+      photoURL: "https://randomuser.me/api/portraits/men/20.jpg"
+    },
+    {
+      id: "b",
+      name: "Tom",
+      photoURL: "https://randomuser.me/api/portraits/men/22.jpg"
+    }
+  ]
+};
+
 const EventDetailedPage = () => {
   return (
     <Grid>
       <Grid.Column width={10}>
-        <EventDetailedHeader></EventDetailedHeader>
-        <EventDetailInfo></EventDetailInfo>
+        <EventDetailedHeader event={event}></EventDetailedHeader>
+        <EventDetailInfo event={event}></EventDetailInfo>
         <EventDetailChat></EventDetailChat>
       </Grid.Column>
       <Grid.Column width={6}>
-        <EventDetailedSidebar></EventDetailedSidebar>
+        <EventDetailedSidebar
+          attendees={event.attendees}
+        ></EventDetailedSidebar>
       </Grid.Column>
     </Grid>
   );
