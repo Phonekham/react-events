91dec168dc42caa716945593d39fe4cca63ab3cc
Phonekham <phone.madridista@gmail.com>
2019-11-18 14:24:56 +0700

    fix appjs path (temporary)

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,6 +1,6 @@
 import React, { Fragment } from "react";
-import { Route } from "react-router-dom";
 import { Container } from "semantic-ui-react";
+import { Route } from "react-router-dom";
 
 import NavBar from "../../features/nav/NavBar/NavBar";
 import HomePage from "../../features/home/HomePage";
@@ -16,24 +16,17 @@ class App extends React.Component {
   render() {
     return (
       <Fragment>
-        <Route exact path="/" component={HomePage}></Route>
-        <Route
-          path="/(.+)"
-          render={() => (
-            <Fragment>
-              <NavBar></NavBar>
-              <Container className="main">
-                <Route exact path="/events" component={EventDashboard}></Route>
-                <Route path="/events/:id" component={EventDetailedPage}></Route>
-                <Route path="/people" component={PeopleDashboard}></Route>
-                <Route path="/profile/:id" component={UserDetailedPage}></Route>
-                <Route path="/settings" component={SettingsDashboard}></Route>
-                <Route path="/createEvent" component={EventForm}></Route>
-                <Route path="/test" component={TestComponent}></Route>
-              </Container>
-            </Fragment>
-          )}
-        />
+        <Route exact path="/" component={HomePage} />
+        <NavBar></NavBar>
+        <Container className="main">
+          <Route exact path="/events" component={EventDashboard}></Route>
+          <Route path="/events/:id" component={EventDetailedPage}></Route>
+          <Route path="/people" component={PeopleDashboard}></Route>
+          <Route path="/profile/:id" component={UserDetailedPage}></Route>
+          <Route path="/settings" component={SettingsDashboard}></Route>
+          <Route path="/createEvent" component={EventForm}></Route>
+          <Route path="/test" component={TestComponent}></Route>
+        </Container>
       </Fragment>
     );
   }
