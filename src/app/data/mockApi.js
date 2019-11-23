bfb6c92412dfbbc3f176741c16fdf3d8236c1b47
Phonekham <phone.madridista@gmail.com>
2019-11-23 16:46:55 +0700

    create a mock api

---
+++ src/app/data/mockApi.js
@@ -0,0 +1,9 @@
+const delay = ms => {
+  return new Promise(resolve => setTimeout(resolve, ms));
+};
+
+export const fetchSampleData = () => {
+  return delay(1000).then(() => {
+    return Promise.resolve(sampleData);
+  });
+};
